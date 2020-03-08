51e88c765dc5b5fd7567740f38a029152da1e554
crownbackend <crownbackend@gmail.com>
2020-03-08 01:39:35 +0100

    variable and const

--- main.cpp
+++ main.cpp
@@ -1,11 +1,47 @@
 #include <iostream>
 #define NOMINMAX
 #include <Windows.h>
+#include <string>
 
 int main() 
 {
 	SetConsoleOutputCP(1252);
-	std::cout << "Il m'a demandé \" Comment vas - tu ? \"" << std::endl;
-	std::cout << "j'ai " << 2536 << " ans";
+    std::cout << "Commutativité :" << std::endl;
+    std::cout << "2 + 3 = " << 2 + 3 << std::endl;
+    std::cout << "3 + 2 = " << 3 + 2 << std::endl;
+    
+    //déclaration de variable 
+    int number { 25 };
+    std::string text { "la question numéro : " };
+    float flottan { 125.365 };
+    std::cout << text << number << flottan;
+
+    // déclaration de constante (impossible de la modifier)
+    double const pi { 3.141592 };
+    std::cout << pi << std::endl;
+
+    // modification des variable 
+    int entier { 30 };
+    std::cout << "mon entier vaut" << --entier << std::endl;
+    entier = entier + 60;
+    std::cout << "mon entier vaut" << ++entier << std::endl;
+
+    // type de variable automatique
+    auto ent { 10 };
+    std::cout << ent << std::endl;
+
+    // Écrivez cette ligne une seule fois.
+    using namespace std::literals;
+
+    // Puis vous pouvez déclarer autant de chaînes de caractères que vous voulez.
+    auto chaine{ "Du texte."s };
+    auto autre_chaine{ "Texte alternatif."s };
+    auto encore_une_chaine{ "Allez, un dernier pour la route."s };
+
+    // permettre a l'utilisateur d'écrire son age
+    std::cout << "Quel est votre age ?" << std::endl;
+    int age{ 0 };
+    std::cin >> age;
+    std::cout << "Tu a : " << age << " ans";
 	return 0;
 }
